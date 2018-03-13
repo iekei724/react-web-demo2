b387f561a177ff61ad81f4eff9eff3362387484a
iekei724 <iekei724@gmail.com>
2018-03-13 21:11:27 +0800

    demo2

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  //const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">目前 <span>{expenseCount}</span> 筆消費 總共 <span>{formattedExpensesTotal}</span>元</h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
